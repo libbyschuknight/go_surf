b768999abd90b2218651139b65edc4c34d9527ec
libbyschuknight <libbysk@gmail.com>
2016-08-21 15:54:39 +1200

    Add actions and views

--- app/controllers/surf_spots_controller.rb
+++ app/controllers/surf_spots_controller.rb
@@ -4,4 +4,34 @@ class SurfSpotsController < ApplicationController
     @surf_spot = SurfSpot.new
   end
 
+  def create
+    @surf_spot = SurfSpot.new(surf_spot_params)
+
+    @surf_spot.save
+    redirect_to @surf_spot
+  end
+
+  def show
+    @surf_spot = SurfSpot.find(params[:id])
+  end
+
+  def index
+    @surf_spots = SurfSpot.all
+  end
+
+  private
+
+    def surf_spot_params
+      params.require(:surf_spot).permit(
+        :name,
+        :description,
+        :place,
+        :region,
+        :country,
+        :latitude,
+        :longtitude,
+        :stoke_rating
+      )
+    end
+
 end
